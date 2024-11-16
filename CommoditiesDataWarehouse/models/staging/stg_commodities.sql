ec33c784f382118728eca3f6dcd727c4c10e2f98
Fabricio Cruz Mendes <fabriciodev@outlook.com>
2024-11-16 20:07:22 -0300

    Criação do staging no dbt

---
+++ CommoditiesDataWarehouse/models/staging/stg_commodities.sql
@@ -0,0 +1,21 @@
+-- import
+with source as (
+    select
+        "Date",
+        "Close",
+        "ticker_id"
+    from
+        {{source ('dbcommodities_1b1h', 'commodities')}}
+),
+
+--renamed
+renamed as (
+    select
+        cast("Date" as date) as data,
+        "Close" as valor_fechamento,
+        "ticker_id" as Ticker
+    from
+        source
+)
+-- select * from
+select * from renamed
